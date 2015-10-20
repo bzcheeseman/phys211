01c61c7a9540f0efa8d95faa7a1d701713d53cc3
bzcheeseman <alachapelle@uchicago.edu>
2015-10-20 09:35:41 -0500

    1445351737

--- gitadd.sh
+++ gitadd.sh
@@ -5,28 +5,17 @@ target="$2"
 
 if [ !"$1" & !"$2" ] ; then
 	echo "Usage: ./gitadd.sh <current_exp> <target> <commit message>"
-
-elif [ !"2" & "$1" ] ; then
-	if [ !"$3" ] ; then
-		git commit -am "$(date +%Y%m%d-%H%M%S)"
-	else
-		git commit -am "$3" 
-	fi
-
-	git push -u origin master
-
-else
-	cd "$current_exp/$target"
-	pdflatex "$target.tex"
-
-	cd "$(id -un)/desktop/phys211"
-
-
-	if [ !"$3" ] ; then
-		git commit -am "$(date +%Y%m%d-%H%M%S)"
-	else
-		git commit -am "$3" 
-	fi
-
-	git push -u origin master
 fi
+
+cd "$current_exp/$target"
+pdflatex "$target.tex"
+
+cd "$(id -un)/desktop/phys211"
+
+if [ !"$3" ] ; then
+	git commit -am "$(date +%Y%m%d-%H%M%S)"
+else
+	git commit -am "$3" 
+fi
+
+git push -u origin master
