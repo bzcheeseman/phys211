28f1c4b11628db6e5db4e0d05d130b4fa78c5751
bzcheeseman <alachapelle@uchicago.edu>
2015-11-16 20:22:45 -0600

    added graphs and fit information, as well as data from the fits including covariance matrix

--- Single_Photon/data_processing.py
+++ Single_Photon/data_processing.py
@@ -9,12 +9,12 @@ def main(file):
 	voltages, incident, coinc12, coinc13 = loadtxt("data/"+file, unpack=True, skiprows=1)
 	print file
 
-	##Sinusoidal Fit, coinc12##
+	##Sinusoidal Fit, coinc12 - all##
 
 	def sin(x, *p):
 		return p[0]*np.sin(p[1]*x+p[2]) + p[0]*np.cos(p[1]*x+p[3]) + p[4]
 
-	p0 = [353, .6, 6, -.7, 903]
+	p0 = [353, .6, 6, -.7, 3100]
 
 	popt, pcov = curve_fit(sin, voltages, coinc12, p0 = p0, maxfev=int(2e6))
 
@@ -26,64 +26,105 @@ def main(file):
 	chisq=np.sum((coinc12-yFit)**2/np.sqrt(coinc12)**2)/(len(coinc12)-len(p0))
 	print chisq
 
+	##Sinusoidal Fit, coinc13 - distinguishable + indistinguishable##
+
+	p01 = [353, .6, 6, -.7, 3100]
+
+	popt1, pcov1 = curve_fit(sin, voltages, coinc13, p0 = p0, maxfev=int(2e6))
+
+	print popt1
+
+	yFit1 = sin(voltages, *popt1)
+	yuFit1 = sin(voltages, *p01)
+
+	chisq1=np.sum((coinc13-yFit1)**2/np.sqrt(coinc13)**2)/(len(coinc13)-len(p01))
+	print chisq1
+
 	##Linear Fit, coinc12##
 
-	p, V = np.polyfit(voltages, coinc13, 1, cov=True)
+	p, V = np.polyfit(voltages, coinc12, 1, cov=True)
 
 	lin=np.poly1d(p)
 
-	lin_chisq = np.sum((coinc13-lin(voltages))**2/np.sqrt(coinc13)**2)/(len(coinc13)-2)
+	lin_chisq = np.sum((coinc12-lin(voltages))**2/np.sqrt(coinc12)**2)/(len(coinc12)-2)
 	print lin_chisq
 
+	##Linear Fit, coinc13##
+
+	p1, V1 = np.polyfit(voltages, coinc13, 1, cov=True)
+
+	lin1=np.poly1d(p1)
+
+	lin_chisq1 = np.sum((coinc13-lin1(voltages))**2/np.sqrt(coinc13)**2)/(len(coinc13)-2)
+	print lin_chisq1
+
 	##save chi squared, fit parameters and error, etc. to data file##
-	parameter_names=["A", "$\theta$","$\delta_1$", "$\delta_2$", "C"]
 	data = np.transpose(np.vstack([popt, np.sqrt(np.absolute(pcov))]))
+	data1 = np.transpose(np.vstack([popt1, np.sqrt(np.absolute(pcov1))]))
+
+	splitter = np.zeros(len(data[0]))
+	#splitter[:] = '%'
+
 	linear = np.zeros_like(data)
-	linear[0,0] = p[0]
-	linear[1,0] = p[1]
-	linear[0,1] = V[0,0]
-	linear[0,2] = V[0,1]
-	linear[1,1] = V[1,0]
-	linear[1,2] = V[1,1]
-	data = np.vstack([data, linear])
+	linear1 = np.zeros_like(data1)
+	linear1[0,0] = p[0]
+	linear1[1,0] = p[1]
+	linear1[0,1] = V[0,0]
+	linear1[0,2] = V[0,1]
+	linear1[1,1] = V[1,0]
+	linear1[1,2] = V[1,1]
 
-	for i in range(0, 10):
-		try:
-			if data[i,0] == 0.0:
-				data = np.delete(data, i, 0)
-		except IndexError:
-			pass
+	linear[0,0] = p1[0]
+	linear[1,0] = p1[1]
+	linear[0,1] = V1[0,0]
+	linear[0,2] = V1[0,1]
+	linear[1,1] = V1[1,0]
+	linear[1,2] = V1[1,1]
 
-	data = np.delete(data, -1, 0)
+	data = np.vstack([data, splitter, data1, splitter, linear, splitter, linear1])
 
-	os.remove("data/fitparms.csv")
-
-	np.savetxt("data/fitparms.csv", data)
+	#np.savetxt("data/fitparms_"+os.path.splitext(file)[0]+".csv", 
+	#	data)
 
 	##Plotting##
 
-	plt.figure(figsize=(10,10))
+	plt.figure(figsize=(15,10))
 	plt.errorbar(voltages, coinc12, xerr=0.1*np.ones_like(voltages), 
 		yerr=np.sqrt(coinc12), fmt='o', ms=0.1, label="Raw Data, %s" % os.path.splitext(file)[0])
 	plt.errorbar(voltages, coinc13, xerr=0.1*np.ones_like(voltages), 
 		yerr=np.sqrt(coinc13), fmt='o', ms=0.1, label="Raw Data, %s" % os.path.splitext(file)[0])
 	plt.plot(voltages, yFit, linewidth=2, alpha=0.8, color='r', 
 		label="Sinusoidal Fit to Interference Pattern")
-	#plt.plot(voltages, yuFit, label="guesses")
-	plt.plot(voltages, lin(voltages), linewidth=np.sqrt(V[1,1]), alpha=.1, 
-		color='r', label="Variance in Linear Fit")
+	#plt.plot(voltages, lin(voltages), linewidth=np.sqrt(V[1,1]), alpha=.1, 
+	#	color='r', label="Variance in Linear Fit")
 	plt.plot(voltages, lin(voltages), 'r', label="Linear Fit")
+
+	plt.plot(voltages, yFit1, linewidth=2, alpha=0.8, color='r')
+	#plt.plot(voltages, lin1(voltages), linewidth=np.sqrt(V1[1,1]), alpha=.1, 
+	#	color='r', label="Variance in Linear Fit")
+	plt.plot(voltages, lin1(voltages), 'r')
 	#sinwave
-	plt.text(31,2000,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
-	plt.text(31,1750,r"$%.1f\sin(%.1f V + (%.1f))$"\
+	plt.text(43,3075,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
+	plt.text(43,3000,r"$%.1f\sin(%.1f V + (%.1f))$"\
 					"\n"\
 					r"$ + %.1f\cos(%.1f V + (%.1f)) + %.1f$" % 
 		(popt[0], popt[1], popt[2], popt[0], popt[1], popt[3], popt[4]))
-	plt.text(31, 1600, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq, len(coinc12)-len(p0)))
+	plt.text(43, 2950, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq, len(coinc12)-len(p0)))
+	##1-3
+	plt.text(43,3400,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
+	plt.text(43,3325,r"$%.1f\sin(%.1f V + (%.1f))$"\
+					"\n"\
+					r"$ + %.1f\cos(%.1f V + (%.1f)) + %.1f$" % 
+		(popt1[0], popt1[1], popt1[2], popt1[0], popt1[1], popt1[3], popt1[4]))
+	plt.text(43, 3275, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq1, len(coinc13)-len(p0)))
 	#linear
-	plt.text(31, 3300, r"$AV + B$")
-	plt.text(31, 3200, r"$%.2fV + %.2f$" % (p[0], p[1]))
-	plt.text(31, 3090, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq, len(coinc13)-2))
+	plt.text(31, 3075, r"$AV + B$")
+	plt.text(31, 3025, r"$%.2fV + %.2f$" % (p[0], p[1]))
+	plt.text(31, 2975, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq, len(coinc12)-2))
+	##1-3
+	plt.text(31, 3400, r"$AV + B$")
+	plt.text(31, 3350, r"$%.2fV + %.2f$" % (p1[0], p1[1]))
+	plt.text(31, 3300, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq1, len(coinc13)-2))
 
 	plt.legend(loc=0)
 	plt.title("Piezo Modulation of Interference Patterns")
@@ -94,5 +135,5 @@ def main(file):
 
 
 if __name__ == '__main__':
-	main("eraser.tsv")
+	main("distinguishable45-7.tsv")
 
