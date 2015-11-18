a0d006709979641e26e32abcb90858d28b0294b2
bzcheeseman <alachapelle@uchicago.edu>
2015-11-18 08:41:15 -0600

    All done - this is the version that went to the grader

--- Single_Photon/data_processing.py
+++ Single_Photon/data_processing.py
@@ -93,38 +93,38 @@ def main(file):
 		yerr=np.sqrt(coinc12), fmt='o', ms=0.1, label="Raw Data, %s" % "Coincidences 1-2")
 	plt.errorbar(voltages, coinc13, xerr=0.1*np.ones_like(voltages), 
 		yerr=np.sqrt(coinc13), fmt='o', ms=0.1, label="Raw Data, %s" % "Coincidences 1-3")
-	plt.plot(voltages, yFit, linewidth=2, alpha=0.8, color='b', 
+	plt.plot(voltages, yFit, linewidth=2, alpha=0.8, color='r', 
 		label="Sinusoidal Fit to Interference Pattern")
 	#plt.plot(voltages, lin(voltages), linewidth=np.sqrt(V[1,1]), alpha=.1, 
 	#	color='r', label="Variance in Linear Fit")
-	plt.plot(voltages, lin(voltages), 'r', label="Linear Fit")
+	plt.plot(voltages, lin(voltages), 'b', label="Linear Fit")
 
 	plt.plot(voltages, yFit1, linewidth=2, alpha=0.8, color='b')
-	#plt.plot(voltages, lin1(voltages), linewidth=np.sqrt(V1[1,1]), alpha=.1, 
-	#	color='r', label="Variance in Linear Fit")
+	plt.plot(voltages, lin1(voltages), linewidth=np.sqrt(V1[1,1]), alpha=.1, 
+		color='r', label="Variance in Linear Fit")
 	plt.plot(voltages, lin1(voltages), 'r')
 	#sinwave
-	plt.text(43,1000,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
-	plt.text(43,500,r"$%.1f\sin(%.1f V + (%.1f))$"\
+	plt.text(45,2000,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
+	plt.text(45,1700,r"$%.1f\sin(%.1f V + (%.1f))$"\
 					"\n"\
 					r"$ + %.1f\cos(%.1f V + (%.1f)) + %.1f$" % 
 		(popt[0], popt[1], popt[2], popt[0], popt[1], popt[3], popt[4]))
-	plt.text(44, 200, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq, len(coinc12)-len(p0)))
+	plt.text(46, 1500, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq, len(coinc12)-len(p0)))
 	##1-3
-	plt.text(43,6800,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
-	plt.text(43,6300,r"$%.1f\sin(%.1f V + (%.1f))$"\
+	plt.text(43,3300,r"$A\sin(\theta V + \delta_1) + A\cos(\theta V + \delta_2) + C$")
+	plt.text(43,3000,r"$%.1f\sin(%.1f V + (%.1f))$"\
 					"\n"\
 					r"$ + %.1f\cos(%.1f V + (%.1f)) + %.1f$" % 
 		(popt1[0], popt1[1], popt1[2], popt1[0], popt1[1], popt1[3], popt1[4]))
-	plt.text(44, 6000, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq1, len(coinc13)-len(p0)))
+	plt.text(44, 2800, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (chisq1, len(coinc13)-len(p0)))
 	#linear
 	plt.text(31, 1000, r"$AV + B$")
 	plt.text(31, 800, r"$%.2fV + %.2f$" % (p[0], p[1]))
-	plt.text(31, 500, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq, len(coinc12)-2))
+	plt.text(31, 600, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq, len(coinc12)-2))
 	##1-3
-	plt.text(31, 6800, r"$AV + B$")
-	plt.text(31, 6600, r"$%.2fV + %.2f$" % (p1[0], p1[1]))
-	plt.text(31, 6300, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq1, len(coinc13)-2))
+	plt.text(31, 3300, r"$AV + B$")
+	plt.text(31, 3100, r"$%.2fV + %.2f$" % (p1[0], p1[1]))
+	plt.text(31, 2900, r"$\chi^2/\nu = %.2f, \, \nu = %.0f$" % (lin_chisq1, len(coinc13)-2))
 
 	plt.legend(loc=0)
 	plt.title("Piezo Modulation of Interference Patterns")
@@ -135,5 +135,5 @@ def main(file):
 
 
 if __name__ == '__main__':
-	main("indistinguishable.tsv")
+	main("eraser.tsv")
 
