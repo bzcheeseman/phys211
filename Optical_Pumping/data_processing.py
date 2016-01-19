19d1469a396488b618ec5ed7ab4254870e28d7a1
bzcheeseman <alachapelle@uchicago.edu>
2016-01-19 10:50:12 -0600

    few corrections

--- Optical_Pumping/data_processing.py
+++ Optical_Pumping/data_processing.py
@@ -4,24 +4,3 @@ from matplotlib import rc
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
-
-def main():
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
