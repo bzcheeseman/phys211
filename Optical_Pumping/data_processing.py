ef04066e7e4117a6e77b63f7a02251e3ef799700
Aman <bzcheeseman@gmail.com>
2016-01-21 13:18:10 -0600

    ready for day 2, hopefully

--- Optical_Pumping/data_processing.py
+++ Optical_Pumping/data_processing.py
@@ -4,3 +4,11 @@ from matplotlib import rc
 
 rc('font', **{'family': 'serif', 'serif': ['Computer Modern']})
 rc('text', usetex=True)
+
+data = genfromtxt("data/s4_3_scope_trace.tsv", skip_header=1)
+data = delete(data, data[:,2], 1)
+
+figure(figsize = (8, 8))
+plot(data[:,1], data[:,0])
+plot(data[:,1], .01*data[:,2])
+show()
