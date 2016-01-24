6b75736b8aac9e63a35307d92c7508a10fb79153
Aman <bzcheeseman@gmail.com>
2016-01-23 22:44:12 -0600

    a few more calculations, still need to check gyromagnetic ratio

--- Optical_Pumping/data_processing.py
+++ Optical_Pumping/data_processing.py
@@ -25,7 +25,7 @@ def larmor_fit(dataset):
     def sin_function(x, A, w, d, c):
         return A*np.sin(w*x + d) + c
 
-    p = [.01, 80e3, 22*np.pi/24, .035]
+    p = [.01, 150e3, np.pi/2, .035]
 
     popt, pcov = curve_fit(sin_function, sel_xdata, sel_ydata, p0 = p)
 
@@ -42,10 +42,37 @@ def larmor_fit(dataset):
     plt.plot(sel_xdata, yFit, 'r', lw = 4, alpha = .7)
     plt.xlabel("time (s)")
     plt.ylabel("signal amplitude (-mV)")
-    plt.savefig("plots/(%s.%s).png" % (dset[0].split("/")[1], dset[1]))
+    plt.savefig("plots/({}.{}).png".format(dset[0].split("/")[1], dset[1]))
 
-    # save the fit parameters into a file to plot/fit
+    with open('fitparms_{}.{}.tsv'.format(dset[0].split("/")[1], dset[1]), 'w')\
+      as f:
+        for key in sorted(parms.keys()):
+            f.write("{}\t{}\n".format(key, parms[key]))
+
+def gyromagnetic_ratio_fit():
+    data20 = np.genfromtxt("fitparms_larmor_y0.020A.tsv", usecols=(1))
+    data33 = np.genfromtxt("fitparms_larmor_y0.033A.tsv", usecols=(1))
+    data40 = np.genfromtxt("fitparms_larmor_y0.040A.tsv", usecols=(1))
+    data45 = np.genfromtxt("fitparms_larmor_y0.045A.tsv", usecols=(1))
+    data55 = np.genfromtxt("fitparms_larmor_y0.055A.tsv", usecols=(1))
+    data65 = np.genfromtxt("fitparms_larmor_y0.065A.tsv", usecols=(1))
+
+    Is = [.02, .033, .04, .045, .055, .065]
+
+    freqs = [data20[1], data33[1], data40[1], data45[1], data55[1], data65[1]]
+
+    def lin(x, A, B):
+        return A*x + B
+
+    popt, pcov = curve_fit(lin, Is, freqs, p0 = np.polyfit(Is, freqs, 1))
+
+    yFit = np.poly1d(popt)
+
+    print yFit
+
+    plt.plot(Is, freqs, 'o')
+    plt.plot(Is, yFit(Is), 'r')
 
 
 if __name__ == '__main__':
-    larmor_fit("data/larmor_y0.020A.tsv")
+    gyromagnetic_ratio_fit()
